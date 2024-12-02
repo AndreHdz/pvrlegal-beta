88491882972c7b8175f38fdc8c8e583a1740f891
Andre Hdz <Andre Hdz>
2024-12-02 00:13:57 -0600

    avance

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'alt' : '#d9f2ff',
+			'alt2' : '#389ecc',
+			'alt3' : '#1a4979',
+			'alt4' : '#389ecc',
+			'alt5' : '#223448',
+			'alt6' : '#3e6382',
+			'alt7' : '#24343f',
+			'alt8' : '#1a4979',
+			'white' : '#fff',
+			'grey' : '#3B3A3E'
+		},
+		extend: {},
+	},
+	plugins: [
+		require('flowbite/plugin')
+	],
+}
